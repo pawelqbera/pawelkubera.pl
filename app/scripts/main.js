c8d7539d36c8888c3e49e045cac95ce4f3935d2f
pawelqbera <pawelqbera@gmail.com>
2015-08-11 16:02:11 +0200

    update files

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,11 +1,12 @@
 $(document).ready(function () {
 	'use strict';
-	
+	var hash = null;
 	var windowHeight = null;
 	var translateY = 0;
 	var pageWrapHeight = null;
 	var sectionNavHeight = null;
-
+	
+	resetHash();
 	updateHeights();
 
 	/**
@@ -20,36 +21,62 @@ $(document).ready(function () {
 	*/
 	$(window).on('resize', function() {
 		updateHeights();
-	}, false);	
-
-	/**
-	*  Sets active link in Bootstrap menu on hash change
-	*/
-	$(window).on('hashchange', function(e) {
-		$('.nav a').parents('li').removeClass('active');
-		$('.nav a[href="' + this.location.hash + '"]').parents('li').addClass('active');
-		$('.section-nav a').children('span').removeClass('point-active');
-		$('.section-nav a[href="' + this.location.hash + '"]').children('span').addClass('point-active');		
-	});
+	}, false);
 
 	/**
 	*  Handles the mousewheel event and scroll up/down the sections
 	*/
-	$(document).mousewheel(function(event) { 
-		var hash = null;
+	$(window).on('mousewheel', function(event) {	
+		clearTimeout($.data(this, 'scrollTimer'));	
+		$.data(this, 'scrollTimer', setTimeout(function() {
+	   		if(event.deltaY < 0 && translateY * -1 < (pageWrapHeight - windowHeight)){
+	    		translateY -= windowHeight;
+	   			$('.current').next().addClass('current').prev().removeClass('current');
+	   		}else if (event.deltaY > 0 && translateY < 0){
+	   			translateY += windowHeight;
+				$('.current').prev().addClass('current').next().removeClass('current');
+	   		}
+	   		hash = $('.current').attr("data-anchor");
+			$(".page-wrap").css({transform: 'translateY(' + translateY + 'px)'});			
+			history.pushState(null, null, '#' + hash);		
+			return onHashChange();
+		}, 250));
+	});
 
-   		if(event.deltaY < 0 && translateY * -1 < (pageWrapHeight - windowHeight)){
-		    translateY -= windowHeight;		
-			$('.current').next().addClass('current').prev().removeClass('current');
-   			hash = $('.current').attr("id");
-   			//window.location.hash = hash;		
-   		}else if (event.deltaY > 0 && translateY < 0){
-   			translateY += windowHeight;
-			$('.current').prev().addClass('current').next().removeClass('current');
-   			hash = $('.current').attr("id");
-   			window.location.hash = hash;   			
-   		}
-   		$(".page-wrap").css({transform: 'translateY(' + translateY + 'px)'});
+	$(window).on('hashchange', function(event) {
+		if(window.location.hash !== "") {
+			onHashChange();
+		}
+	});
+
+	/**
+	*  Handles the 
+	*/
+	$('.section-nav a').click(function(event){
+		event.preventDefault();
+		var targetSectionAnchor = $(this).attr("href").replace('#','');
+		var index = $('#' + targetSectionAnchor).index();
+		translateY = index * windowHeight * -1;
+		$(".page-wrap").css({transform: 'translateY(' + translateY + 'px)'});
+		$('.current').removeClass('current');
+		$('#' + targetSectionAnchor).addClass('current');
+		history.pushState(null, null, '#' + targetSectionAnchor);
+		return onHashChange();
+	});
+
+	/**
+	*  Handles the 
+	*/
+	$('.nav a').click(function(event){
+		event.preventDefault();
+		var targetSectionAnchor = $(this).attr("href").replace('#','');
+		var index = $('#' + targetSectionAnchor).index();
+		translateY = index * windowHeight * -1;
+		$(".page-wrap").css({transform: 'translateY(' + translateY + 'px)'});
+		$('.current').removeClass('current');
+		$('#' + targetSectionAnchor).addClass('current');
+		history.pushState(null, null, '#' + targetSectionAnchor);
+		return onHashChange();
 	});	
 
 	/**
@@ -75,6 +102,18 @@ $(document).ready(function () {
 		setWindowHeight();
 		pageWrapHeight = $(".page-wrap").height();
 		setSectionNavMargin();	
+	}
+
+	function onHashChange() {
+		$('.nav a').parents('li').removeClass('active');
+		$('.nav a[href="' + window.location.hash + '"]').parents('li').addClass('active');
+		$('.section-nav a').children('span').removeClass('point-active');
+		$('.section-nav a[href="' + window.location.hash + '"]').children('span').addClass('point-active');
+	}
+
+	function resetHash() {
+		window.location.href = "#home";
+		history.pushState(null, null, window.location.pathname);
 	}
 
 });
